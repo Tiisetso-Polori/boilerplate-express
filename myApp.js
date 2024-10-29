d9fef41118574a9db470feb213cfddf7e09abd3a
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-10-29 20:36:45 +0200

    recent changes

--- myApp.js
+++ myApp.js
@@ -1,20 +1,77 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+let bodyParser = require('body-parser');
+
+// Logger middleware at the root level
+app.use((req, res, next) => {
+    let logString = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logString);
+    next(); // Important: move to the next middleware or route
+});
+
+//console.log("Hello World");
+
+// app.get("/", (req, res) =>{
+//     res.send('Hello Express');
+//   });
+
+  app.get("/", (req, res) =>{
+    absolutePath= __dirname + '/views/index.html';
+    return res.sendFile(absolutePath);
+  });
+
+  app.use('/public', express.static(__dirname+'/public/style.css'));
 
 
+app.get("/json", (req, res) =>{
+    msgKey = "Hello json";
+    if (process.env.MESSAGE_STYLE === 'uppercase') msg = {"message":msgKey.toUpperCase()};
+    else msg = {"message":msgKey.charAt(0).toUpperCase()+
+        msgKey.toLowerCase().slice(1)};
+    return res.json(msg);
+  });
 
 
+  app.get(
+    "/now",
+    (req, res, next) => {
+      // adding a new property to req object
+      // in the middleware function
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      // accessing the newly added property
+      // in the main function
+      res.send({
+        time : req.time
+      });
+    }
+  );
 
 
+  app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+      echo: word
+    });
+  });
+
+  app.get("/name", (req, res) => {
+    var name = req.query.first+ ' '+req.query.last;
+    //var name = firstName.toString()+' '+lastName.toString()
+    res.json({
+      name
+    });
+  });
+
+  bodyParser.urlencoded({extended: false});
+
+  app.get((req,res, next) => {
 
 
-
-
-
-
-
-
-
+  })
 
 
 
